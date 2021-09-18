4605dee3809ee0309793376f62c06cc4b9be958f
user-bot001 <dron201202@gmail.com>
2021-09-18 12:25:15 +0400

    Добавьте файлы проекта.

---
+++ WinFormsNorthwind/Controller.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace WinFormsNorthwind
+{
+    public static class Controller
+    {
+        public static List<Supplier> GetSuppliers()
+        {
+            try
+            {
+                using (var db = new NorthwindContext())
+                {
+                    var lst = db.Suppliers.ToList();
+                    if (lst != null) return lst;
+                    else return null;
+                }
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+        }
+    }
+}
---
+++ WinFormsNorthwind/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsNorthwind
+{
+    public partial class Form1 : Form
+    {
+        List<Supplier> Suppliers;
+        public Form1()
+        {
+            InitializeComponent();
+            Suppliers = new List<Supplier>();
+            dataGridView1.AutoGenerateColumns = true;
+            dataGridView1.DataSource = bindingSource1;
+            bindingSource1.DataSource = Suppliers;
+            btnUpdateTable.Click += BtnUpdateTable_Click;
+        }
+
+        private void BtnUpdateTable_Click(object sender, EventArgs e)
+        {
+            UpdateTable();
+        }
+
+        void UpdateTable ()
+        {
+            Suppliers.Clear();
+            var lst = Controller.GetSuppliers();
+            if (lst != null && lst.Count> 0)
+            {
+                Suppliers.AddRange(lst);
+                bindingSource1.ResetBindings(false);
+            }
+
+        }
+    }
+}
